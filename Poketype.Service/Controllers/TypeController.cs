33ee1ccd1cdbb0867e5bd7d8f8bb4636a41a8233
Pasquale Caiazzo <plcaiazzo@gmail.com>
2019-11-29 17:12:00 -0600

    added lib for postgres orm

---
+++ Poketype.Service/Controllers/TypeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Poketype.Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TypeController : ControllerBase
+    {
+    }
+}
